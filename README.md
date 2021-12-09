525f67f3e2c7747e2899cad9df04be4ed9beba82
Michael Lu <mlu0708@mit.edu>
2021-12-09 16:36:07 -0500

    Shit.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # HackMIT CSS Tutorial
 
+Tutorial Recording: https://youtu.be/QiqmbbrNW6k
+
 ![John Xina](https://raw.githubusercontent.com/techx/css_setup/master/src/assets/xina.png)
--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
 
+import john from './assets/john.png';
+import ani from './assets/ani.png';
+import gnat from './assets/gnat.png';
+
 function App() {
   return (
     <div id="wrapper">
-      <div>deez</div>
+        <div className="person">
+          <img src={gnat} alt="hooman" />
+        </div>
+        
+        <div className="person">
+          <img src={john} alt="hooman" />
+        </div>
+        
+        <div className="person">
+          <img src={ani} alt="hooman" />
+        </div>
     </div>
   );
 }
